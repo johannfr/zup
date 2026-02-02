bab4d291e9204cf9fc8abede3348502d45f6d940
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2026-02-02 09:43:16 +0000

    Fixing shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   packages = [
+    pkgs.rustc
+    pkgs.cargo
     pkgs.python3
-    pkgs.python312Packages.pyside6
-    pkgs.python312Packages.appdirs
-    pkgs.python312Packages.pendulum
-    pkgs.python312Packages.requests
+    pkgs.python3Packages.pyside6
+    pkgs.python3Packages.appdirs
+    pkgs.python3Packages.pendulum
+    pkgs.python3Packages.requests
   ];
 
   env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
